d06adf3d054c3371b8ae4a9d154c044bbbcefc60
sei-triangle <sei-triangle@outlook.com>
2014-09-20 19:20:35 +0900

    Initial Commit

---
+++ instagram-nyanchan22/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instagram-nyanchan22
+//
+//  Created by sei on 2014/09/20.
+//  Copyright (c) 2014年 sei-triangle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ instagram-nyanchan22Tests/instagram_nyanchan22Tests.swift
@@ -0,0 +1,36 @@
+//
+//  instagram_nyanchan22Tests.swift
+//  instagram-nyanchan22Tests
+//
+//  Created by sei on 2014/09/20.
+//  Copyright (c) 2014年 sei-triangle. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class instagram_nyanchan22Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
